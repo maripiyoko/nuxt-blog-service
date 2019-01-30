21bff612142d608106a61cea093dc3c031454529
Mariko <maripiyok@gmail.com>
2019-01-30 20:39:09 +0900

    Add header component and default layout.

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <el-menu mode="horizontal" :router="true">
+    <el-menu-item index="1" style="pointer-events:none;">
+      Nuxt Blog Service
+    </el-menu-item>
+  </el-menu>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <nuxt />
+    <TheHeader />
+    <div class="wrapper">
+      <nuxt class="container" />
+    </div>
   </div>
 </template>
+
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
