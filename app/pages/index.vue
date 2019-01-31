9628a32a1b715710195ba9d115e581b0783e0c83
Mariko <maripiyok@gmail.com>
2019-01-31 22:28:56 +0900

    view

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,6 +4,47 @@
       <div slot="header" class="clearfix">
         <span>Login</span>
       </div>
+    
+      <form>
+        <div class="form-content">
+          <span>User ID</span>
+          <el-input v-model="formData.id" />
+        </div>
+        <div class="form-content">
+          <el-checkbox v-model="isCreateMode">
+            アカウントを作成する
+          </el-checkbox>
+        </div>
+        <div class="text-right">
+          <el-button type="primary">
+            {{ buttonText }}
+          </el-button>
+        </div>
+      </form>
     </el-card>
   </section>
 </template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      isCreateMode: false,
+      formData: {
+        id: ''
+      }
+    }
+  },
+  computed: {
+    buttonText() {
+      return this.isCreateMode ? '新規登録' : 'ログイン'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-content {
+  margin: 16px 0;
+}
+</style>
---
+++ app/pages/posts/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="container posts-page">
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>新着投稿</span>
+      </div>
+      <el-table :data="showPosts" style="width: 100%" class="table">
+        <el-table-column prop="title" label="タイトル"></el-table-column>
+        <el-table-column prop="user.id" label="投稿者" width="180"></el-table-column>
+        <el-table-column prop="created_at" label="投稿日時" width="240"></el-table-column>
+      </el-table>
+    </el-card>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    showPosts() {
+      return [
+        {
+          id: '001',
+          title: 'Nuxt.js Application',
+          body: 'テスト',
+          created_at: '2019/01/31 12:00:00',
+          user: {
+            id: 'potato'
+          }
+        },
+        {
+          id: '002',
+          title: 'Development Vue.js',
+          body: 'テスト',
+          created_at: '2019/01/31 15:00:00',
+          user: {
+            id: 'potato'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
